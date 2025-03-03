34ae0d778543a4df8b9ec8b728032f2eb0248234
kiruthikanatarajan18@gmail.com <kiruthikanatarajan18@gmail.com>
2025-03-03 17:34:40 +0530

    Added personal-api.py

---
+++ personal-api.py
@@ -0,0 +1,17 @@
+from flask import Flask 
+app = Flask(__name__)
+@app.route('/')
+def personal_details():
+    return 'Hello,World!'
+
+@app.route('/name')
+def get_name():
+    return 'Kiruthika Natarajan'
+@app.route('/regno')
+def get_regno():
+    return '22IT018'
+@app.route('/department')
+def get_dept():
+    return 'Information Technology'
+if __name__ == '__main__':
+    app.run(debug=True)
